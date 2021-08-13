36850680c86ea84c31ef817a96527005e7136018
IvanEnclonar <67180207+IvanEnclonar@users.noreply.github.com>
2021-08-13 21:13:54 +0800

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FrontPage from './components/FrontPage.js';
+
+function App() {
+  return (
+    <div className="App">
+        <FrontPage /> 
+    </div>
+  );
+}
+
+export default App;
